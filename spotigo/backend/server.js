1f565113d292c37ce6ecc127bf62b30306932406
Andrew Hu <blueshark567@gmail.com>
2023-04-09 23:58:33 -0400

    commit 1

--- spotigo/backend/server.js
+++ spotigo/backend/server.js
@@ -1,12 +1,60 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const app = express();
 require('dotenv').config();
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require('spotify-web-api-node');
 
-
-app.use(express.json());
+const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+
+const spotifyClientId = process.env.SPOTIFY_CLIENT_ID
+const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET
+
+app.post('/spotify/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000/dashboard',
+        clientId: spotifyClientId,
+        clientSecret: spotifyClientSecret,
+        refreshToken: refreshToken
+    });
+
+    spotifyApi.refreshAccessToken()
+    .then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            expiresIn: data.body.expires_in
+        });
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+    });
+});
+
+app.post('/spotify/login', (req, res) => {
+    const code = req.body.code;
+
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000/dashboard',
+        clientId: spotifyClientId,
+        clientSecret: spotifyClientSecret
+    });
+    console.log(code);
+    spotifyApi.authorizationCodeGrant(code)
+    .then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        });
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+    });
+});
 
 app.get('/api/distance', async (req, res) => {
   const { departure, arrival } = req.query;
@@ -25,5 +73,4 @@ app.get('/api/distance', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(8080);
