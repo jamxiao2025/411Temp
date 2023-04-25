51d3392f31ce51ae24f0baa196eac4ddbe583f62
Andrew Hu <blueshark567@gmail.com>
2023-04-25 18:27:42 -0400

    kinda working app

--- spotigo/src/pages/dashboard.js
+++ spotigo/src/pages/dashboard.js
@@ -21,8 +21,10 @@ function Dashboard({ code }) {
   const [arrival, setArrival] = useState("");
   const [distance, setDistance] = useState("");
   const [duration, setDuration] = useState("");
+  const [durationTime, setDurationTime] = useState(0);
   const [tracks, setTracks] = useState([]);
   const [genreOptions, setgenreOptions] = useState();
+  const [selectedGenres, setSelectedGenres] = useState([]);
 
   useEffect(() => {
     if (!accessToken) return;
@@ -33,8 +35,9 @@ function Dashboard({ code }) {
     event.preventDefault();
     try {
       const response = await axios.get(`http://localhost:8080/api/distance?departure=${departure}&arrival=${arrival}`);
-      setDistance(response.data.rows[0].elements[0].distance.text)
-      setDuration(response.data.rows[0].elements[0].duration.text)
+      setDistance(response.data.rows[0].elements[0].distance.text);
+      setDuration(response.data.rows[0].elements[0].duration.text);
+      setDurationTime(response.data.rows[0].elements[0].duration.value * 1000);
     } catch (error) {
       console.error(error);
     }
@@ -69,17 +72,42 @@ function Dashboard({ code }) {
     });
   }, [accessToken]);
 
-  function findSongs() {
+  function findSongs(querySize) {
+    console.log(selectedGenres);
+    if (!durationTime) {
+      window.alert("Please Enter a Route!");
+      return
+    }
+
+    if (selectedGenres.length === 0) {
+      window.alert("Please Select a Genre!");
+      return;
+    }
+
     spotifyApi.getRecommendations({
-      min_energy: 0.4,
-      seed_artists: ['6mfK6Q2tzLMEchAr0e9Uzu', '4DYFVNKZ1uixa6SQTvzQwJ'],
-      min_popularity: 50
+      limit: querySize,
+      // min_energy: 0.4,
+      seed_genres: selectedGenres,
+      // min_popularity: 10
     })
     .then(res => {
-      const data = res.body;
+      const data = res.body.tracks;
+      let playlist = [];
+      let pLength = 0;
+      
+      for (const track of data) {
+        playlist.push(track);
+        pLength += track.duration_ms;
 
+        if (pLength > durationTime) break;
+      }
+
+      console.log(pLength, durationTime);
+
+      (pLength < durationTime) && (querySize < 100) && (playlist.length === querySize) ? 
+      findSongs(querySize + 20) :
       setTracks(
-        data.tracks.map(track => {
+        playlist.map(track => {
           const smallestAlbumImage = track.album.images.reduce( (smallest, image) => {
             if (image.height < smallest.height) return image
             return smallest
@@ -95,6 +123,10 @@ function Dashboard({ code }) {
       );
     })
     .catch(err => console.log("Something went wrong!", err));
+  }
+
+  function handleGenreSelect(e) {
+    setSelectedGenres(e.map(genre => genre.value[0]));
   }
 
   return (
@@ -113,15 +145,16 @@ function Dashboard({ code }) {
       </form>
       <h1>Distance:{distance}</h1>
       <h1>Duration:{duration}</h1>
+      <h1>Genre</h1>
       <Select
         isMulti
         name="genres"
         options={genreOptions}
         className="basic-multi-select"
         classNamePrefix="select"
-        onChange={e => console.log(e)}
+        onChange={e => handleGenreSelect(e)}
       />
-      <button onClick={findSongs}>Find Songs</button>
+      <button onClick={() => findSongs(20)}>Find Songs</button>
       <div style={{overflowy: 'auto'}}>
             {tracks.map(track => (
                 <DisplayTrack track={track} key={track.uri}/>
