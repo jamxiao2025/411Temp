d1869939f3367b8ab4fa7bc152d0afe13d682b9e
Andrew Hu <blueshark567@gmail.com>
2023-04-26 21:04:56 -0400

    login with spotify button styling

--- spotigo/src/pages/spotifyLogin.js
+++ spotigo/src/pages/spotifyLogin.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import {Button} from '@chakra-ui/react';
 
 const AUTH_URL = process.env.REACT_APP_SPOTIFY_AUTH_URL;
 
 export default function Login() {
   return (
-        <a className='btn btn-success btn-lg' href={AUTH_URL} >Login With Spotify</a>
+        <Button onClick={() => window.location.href = (AUTH_URL)} colorScheme="green" size="md" >Login With Spotify</Button>
   )
 }
